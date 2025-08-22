1d65e6b12bc74e887a971b59a589ca0c18f7b8e5
Masum Billah <masumbillah2062003@gmail.com>
2025-08-22 11:01:52 +0600

    queryBuilder create

--- src/app/module/user/user.model.ts
+++ src/app/module/user/user.model.ts
@@ -1,58 +1,58 @@
-
 import { model, Schema } from 'mongoose'
 import { TUser, UserModel } from './user.interface'
 import { role, userStatus } from './user.constant'
 import bcrypt from 'bcrypt'
 
-const userSchema = new Schema<TUser, UserModel>({
-  name: {
-    type: String,
-    required: true,
+const userSchema = new Schema<TUser, UserModel>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    isEmailVerified: {
+      type: Boolean,
+      default: false,
+    },
+    contactNumbar: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: 0,
+    },
+    needsPasswordChange: {
+      type: Boolean,
+      default: true,
+    },
+    passwordChangedAt: {
+      type: Date,
+    },
+    role: {
+      type: String,
+      enum: [...role],
+      default: 'user',
+    },
+    isActive: {
+      type: String,
+      enum: [...userStatus],
+      default: 'in-progress',
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-  },
-  isEmailVerified: {
-    type: Boolean,
-    default: false,
-  },
-  contactNumbar: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    select: 0,
-  },
-  needsPasswordChange: {
-    type: Boolean,
-    default: true,
-  },
-  passwordChangedAt: {
-    type: Date,
-  },
-  role: {
-    type: String,
-    enum: [...role],
-    default: 'user',
-  },
-  isActive: {
-    type: String,
-    enum: [...userStatus],
-    default: 'in-progress',
-  },
-  isDeleted: {
-    type: Boolean,
-    default: false,
-  },
-})
-
-
+  { timestamps: true },
+)
 
 userSchema.post('save', function (doc, next) {
   doc.password = ' '
@@ -71,7 +71,6 @@ userSchema.statics.isPasswordMatched = async function (
   plainTextPassword: string,
   hashedPassword: string,
 ) {
-
   return await bcrypt.compare(plainTextPassword, hashedPassword)
 }
 
